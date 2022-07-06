a32b85c96dbb9c3d5f6aec81a7f0248e97dda145
ChuChencheng <azureternite@live.com>
2022-07-07 03:46:57 +0800

    feat: add get scrolled size by index function

--- demo/index.ts
+++ demo/index.ts
@@ -1,4 +1,4 @@
-import VirtualList from '../src'
+import VirtualList, { GET_SCROLLED_SIZE_POSITION_ENUM } from '../src'
 import './style.css'
 
 const data: Array<{ index: number; height: number}> = []
@@ -14,9 +14,12 @@ const $app = document.getElementById('app')
 
 if ($app) {
   $app.innerHTML = `
-    <div class="virtual-list-viewport">
-      <div class="virtual-list-whole">
-        <div class="virtual-list-item-container"></div>
+    <div>
+      <button class="scroll-to-button">Scroll to</button>
+      <div class="virtual-list-viewport">
+        <div class="virtual-list-whole">
+          <div class="virtual-list-item-container"></div>
+        </div>
       </div>
     </div>
   `
@@ -25,6 +28,7 @@ if ($app) {
 const $viewport: HTMLDivElement | null = document.querySelector('.virtual-list-viewport')
 const $whole: HTMLDivElement | null = document.querySelector('.virtual-list-whole')
 const $itemContainer: HTMLDivElement | null = document.querySelector('.virtual-list-item-container')
+const $scrollToButton: HTMLButtonElement | null = document.querySelector('.scroll-to-button')
 
 const clientHeight = $viewport?.clientHeight || 0
 const dataLength = data.length
@@ -32,14 +36,31 @@ const itemMinSize = 30
 let previousStartIndex = -1
 let previousEndIndex = -1
 
+const virtualListInstance = new VirtualList({
+  viewportSize: clientHeight,
+  dataLength,
+  itemMinSize,
+})
+
+let i = 0
+const positionArray = [
+  GET_SCROLLED_SIZE_POSITION_ENUM.TOP,
+  GET_SCROLLED_SIZE_POSITION_ENUM.MIDDLE,
+  GET_SCROLLED_SIZE_POSITION_ENUM.BOTTOM,
+]
+
+$scrollToButton?.addEventListener('click', () => {
+  const nextPosition = positionArray[i % positionArray.length]
+  const scrollTop = virtualListInstance.getScrolledSizeByIndex(30000, nextPosition)
+  if ($viewport) {
+    $viewport.scrollTop = scrollTop
+  }
+  i++
+})
+
 const handleScroll = () => {
   const scrollTop = $viewport?.scrollTop || 0
-  const { startIndex, endIndex, estimatedRenderCount } = VirtualList.getRange({
-    scrolledSize: scrollTop,
-    viewportSize: clientHeight,
-    dataLength,
-    itemMinSize,
-  })
+  const { startIndex, endIndex } = virtualListInstance.getRange(scrollTop)
   const visibleData = data.slice(startIndex, endIndex)
 
   if ($whole?.style) {
@@ -52,7 +73,7 @@ const handleScroll = () => {
         return `
           <div
             class="virtual-list-item"
-            style="box-sizing:border-box;border:1px solid black;min-height:${30 + (i % 10) * 10}px;width:100%;"
+            style="min-height:${30 + (i % 10) * 10}px;"
           >${d.index}</div>
         `
       }).join('')
@@ -66,13 +87,10 @@ const handleScroll = () => {
       visibleItemRealSizeList.push($item.offsetHeight)
     })
 
-    const offset = VirtualList.getOffset({
+    const offset = virtualListInstance.getOffset({
       scrolledSize: scrollTop,
       startIndex,
       endIndex,
-      estimatedRenderCount,
-      viewportSize: clientHeight,
-      itemMinSize,
       visibleItemRealSizeList,
     })
 
--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,69 @@
+const VIRTUAL_LIST_LOG_PREFIX = '[Virtual List]'
 const DEFAULT_BUFFER_COUNT = 20
 
-export interface IVirtualListGetRangeParameters {
-  scrolledSize: number
+export enum GET_SCROLLED_SIZE_POSITION_ENUM {
+  TOP = 'TOP',
+  MIDDLE = 'MIDDLE',
+  BOTTOM = 'BOTTOM',
+}
+
+export interface IVirtualListOptions {
   viewportSize: number
   dataLength: number
   itemMinSize: number
   bufferCount?: number
 }
 
-export interface IVirtualListGetOffsetParameters extends Omit<IVirtualListGetRangeParameters, 'dataLength'> {
+export interface IVirtualListGetOffsetParameters {
+  scrolledSize: number
   startIndex: number
   endIndex: number
-  estimatedRenderCount: number
   visibleItemRealSizeList: number[]
 }
 
-const VirtualList = {
-  getRange ({
-    scrolledSize,
-    viewportSize,
-    dataLength,
-    itemMinSize,
-    bufferCount = DEFAULT_BUFFER_COUNT,
-  }: IVirtualListGetRangeParameters): { startIndex: number; endIndex: number; estimatedRenderCount: number } {
-    const viewportCount = Math.ceil(viewportSize / itemMinSize)
-    const estimatedRenderCount = viewportCount + bufferCount
+class VirtualList {
+  private estimatedRenderCount!: number
+  
+  private bufferCount!: number
+
+  constructor (private readonly options: IVirtualListOptions) {
+    this.bufferCount = options.bufferCount ?? DEFAULT_BUFFER_COUNT
+
+    if (options.viewportSize < 0) this.throwError('viewportSize')
+    if (options.itemMinSize <= 0) this.throwError('itemMinSize')
+    if (options.dataLength < 0) this.throwError('dataLength')
+    if (this.bufferCount <= 0) this.throwError('bufferCount')
+
+    const viewportCount = Math.ceil(options.viewportSize / options.itemMinSize)
+    this.estimatedRenderCount = viewportCount + this.bufferCount
+  }
+
+  private throwError (field: string) {
+    throw new Error(`${VIRTUAL_LIST_LOG_PREFIX} ${field} invalid`)
+  }
+
+  getRange (scrolledSize: number): { startIndex: number; endIndex: number } {
+    const { bufferCount, estimatedRenderCount } = this
+    const { itemMinSize, dataLength } = this.options
+
     const startIndex = Math.floor(Math.floor(scrolledSize / itemMinSize) / bufferCount) * bufferCount
     const endIndex = Math.min(estimatedRenderCount + startIndex, dataLength)
 
     return {
       startIndex,
       endIndex,
-      estimatedRenderCount,
     }
-  },
+  }
 
   getOffset ({
     scrolledSize,
     startIndex,
     endIndex,
-    estimatedRenderCount,
-    viewportSize,
-    itemMinSize,
     visibleItemRealSizeList,
-    bufferCount = DEFAULT_BUFFER_COUNT,
   }: IVirtualListGetOffsetParameters): number {
+    const { bufferCount, estimatedRenderCount } = this
+    const { itemMinSize, viewportSize } = this.options
+
     const renderCount = endIndex - startIndex
     const isLastScroll = renderCount !== estimatedRenderCount
     let scrollableSize = 1
@@ -58,10 +77,28 @@ const VirtualList = {
       realScrollableSize = visibleItemRealSizeList.slice(0, bufferCount).reduce((sum, current) => sum + current, 0) || scrollableSize
     }
 
-    const offset = scrolledSize - (scrolledSize - startIndex * itemMinSize) * realScrollableSize / scrollableSize
+    const ratio = realScrollableSize > 0 && scrollableSize > 0 ? realScrollableSize / scrollableSize : 1
+    const offset = scrolledSize - (scrolledSize - startIndex * itemMinSize) * ratio
 
     return offset
-  },
+  }
+
+  getScrolledSizeByIndex (dataIndex: number, position: GET_SCROLLED_SIZE_POSITION_ENUM | number): number {
+    const { itemMinSize, viewportSize, dataLength } = this.options
+
+    let percentage = 0
+    if (typeof position === 'number') {
+      percentage = position
+    } else if (position === GET_SCROLLED_SIZE_POSITION_ENUM.MIDDLE) {
+      percentage = 0.5
+    } else if (position === GET_SCROLLED_SIZE_POSITION_ENUM.BOTTOM) {
+      percentage = 1
+    }
+
+    const scrolledSize = dataIndex * itemMinSize - (viewportSize - itemMinSize) * percentage
+    const maxScrolledSize = dataLength * itemMinSize - viewportSize
+    return Math.max(0, Math.min(maxScrolledSize, scrolledSize))
+  }
 }
 
 export default VirtualList
